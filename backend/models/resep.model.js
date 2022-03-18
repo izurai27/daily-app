497ba9e2912b943f128c51ee7121970634b45f75
Izma Raifani <izurai@gmail.com>
2022-03-18 09:50:41 +0700

    add occasion and mealtype field to schema, success to insert ingredients and instruction

--- backend/models/resep.model.js
+++ backend/models/resep.model.js
@@ -12,10 +12,12 @@ const recipeSchema = new schema ({
   thumb : {type: String, require : true},
   portion : {type : Number, require : true},
   cookingStyle : { type: String, require : true},
-  instruction : { type: String, require: true},
+  instruction : [String],
   source : {type:String, require : true},
   ytLink : {type:String, require: true},
-  ingredients : [detailIngredients]
+  ingredients : [detailIngredients],
+  occasion : [String],
+  mealtype : [String]
  })
 
 
