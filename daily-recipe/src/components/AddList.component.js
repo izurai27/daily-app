343fa8f2598d51eb5e938094eb6a2539da3e808d
Izma Raifani <izurai@gmail.com>
2022-03-24 14:36:49 +0700

    add component hero, recipeList, gap, AddList sukses tampil all recipe

---
+++ daily-recipe/src/components/AddList.component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import listImg from '../images/listbelanja.svg'
+
+const AddList = () => {
+  return (
+    <img src={listImg} className="img-thumbnail" alt="listbelanjaImg" style={{width:"35px", height:"35px", border:"none", cursor:"pointer"}}></img>
+  )
+}
+
+export default AddList
